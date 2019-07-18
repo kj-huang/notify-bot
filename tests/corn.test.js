25a5606e3d1f10f59d3a57a137b27047aca32c45
mybaseball52 <mybaseball52@gmail.com>
2019-07-18 12:00:58 +0800

    update corn job tests

--- tests/corn.test.js
+++ tests/corn.test.js
@@ -8,13 +8,13 @@ describe("Test Date Calculator functions", () =>{
         expect(valid).to.be.true;
     })
 
-    it("should validate with cron register at 0 0 1 * * * 2", () =>{
-        let valid = cron.validate('0 0 1 * * * 2');
+    it("should validate with cron register at 0 0 1 * * * Tuesday", () =>{
+        let valid = cron.validate('0 0 1 * * * Tuesday');
         expect(valid).to.be.true;
     })
 
-    it("should validate with cron register at 0 0 1 * * * 5", () =>{
-        let valid = cron.validate('0 0 1 * * * 5');
+    it("should validate with cron register at 0 0 1 * * * Friday", () =>{
+        let valid = cron.validate('0 0 1 * * * Friday');
         expect(valid).to.be.true;
     })
 })
