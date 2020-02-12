a8dc5635f00ce77c3ad18d8f2894d5fe2945d2b0
Kevin <mybaseball52@gmail.com>
2020-02-12 11:00:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,20 +28,17 @@ cron.schedule('0 0 * * *',async () => {
       let now = moment().tz("Asia/Taipei").format("YYYYMMDD");
     
       if(baseService.isRemainThreeDays(now, scheduleDates[0])){
-        lineHelper.pushMarketingMsg2To('Cab8dc815286247966f63012fb4dd64e4');
         lineHelper.pushMarketingMsgTo('Cab8dc815286247966f63012fb4dd64e4');
       } 
 
       else if(baseService.isRemainOneDays(now, scheduleDates[0])){
         lineHelper.pushMsgTo('Cab8dc815286247966f63012fb4dd64e4');
         lineHelper.pushActMsgTo('Cab8dc815286247966f63012fb4dd64e4');
-        lineHelper.pushActivityMsgTo('Cab8dc815286247966f63012fb4dd64e4');
       }
       
       else if(baseService.isToday(now, scheduleDates[0])){
+        lineHelper.pushActivityMsgTo('Cab8dc815286247966f63012fb4dd64e4');
         lineHelper.pushActivityMsg2To('Cab8dc815286247966f63012fb4dd64e4');
-        lineHelper.pushActivityMsg3To('Cab8dc815286247966f63012fb4dd64e4');
-        lineHelper.pushActivityMsg4To('Cab8dc815286247966f63012fb4dd64e4');
       } 
       
       else if(baseService.isDPlusOneDay(now, scheduleDates[0])){
