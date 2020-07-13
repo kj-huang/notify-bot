7a068ea02bf9c1cea4bd27ec03769f2a62abfbcf
Kevin <mybaseball52@gmail.com>
2020-07-13 10:10:43 +0800

    Update line-helper.js
    fix content msg

--- services/line-helper.js
+++ services/line-helper.js
@@ -16,7 +16,7 @@ model.pushMsgTo = async function (ID) {
 }
 
 model.pushActMsgTo = async function (ID) {
-  await LineBot.pushText(ID, '講師組 國龍：提醒你今天需請主講人1.觀看主講人SOP影片 2.請講師當天2050上線測試 3.開始前要求與會者寫FB100字心得');
+  await LineBot.pushText(ID, '活動組 世嵩：提醒您今晚2000要發布有報名的人員Zoom連結，要加上講員講義。Robot 溫馨提醒');
 }
 
 
